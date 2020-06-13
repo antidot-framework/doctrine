8a51a816796e26233b0b2ddabc9a47faac4f5125
Koldo Picaza <kpicaza.sound@gmail.com>
2020-06-13 12:12:10 +0200

    update php and remove deprecated commands

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Antidot Doctrine Integration
 
+Integration library between [Doctrine ORM]() and [Antidot Framework]() using [dasprid/container-interop-doctrine]() factories.
+
+## Install
+
+Install using [composer]().
+
+```bash
+composer require antidot-fw/doctrine
+```
+
+
 
--- src/Container/Config/ConfigProvider.php
+++ src/Container/Config/ConfigProvider.php
@@ -9,8 +9,6 @@ use Antidot\Persistence\Doctrine\Container\AntidotCliDoctrineDelegatorFactory;
 use ContainerInteropDoctrine\ConnectionFactory;
 use ContainerInteropDoctrine\EntityManagerFactory;
 use Doctrine\DBAL\Connection;
-use Doctrine\DBAL\Driver\PDOSqlite\Driver;
-use Doctrine\DBAL\Tools\Console\Command\ImportCommand;
 use Doctrine\DBAL\Tools\Console\Command\ReservedWordsCommand;
 use Doctrine\DBAL\Tools\Console\Command\RunSqlCommand;
 use Doctrine\Migrations\Tools\Console\Command\DiffCommand;
@@ -31,12 +29,9 @@ use Doctrine\ORM\Tools\Console\Command\ClearCache\MetadataCommand;
 use Doctrine\ORM\Tools\Console\Command\ClearCache\QueryCommand;
 use Doctrine\ORM\Tools\Console\Command\ClearCache\QueryRegionCommand;
 use Doctrine\ORM\Tools\Console\Command\ClearCache\ResultCommand;
-use Doctrine\ORM\Tools\Console\Command\ConvertDoctrine1SchemaCommand;
 use Doctrine\ORM\Tools\Console\Command\ConvertMappingCommand;
 use Doctrine\ORM\Tools\Console\Command\EnsureProductionSettingsCommand;
-use Doctrine\ORM\Tools\Console\Command\GenerateEntitiesCommand;
 use Doctrine\ORM\Tools\Console\Command\GenerateProxiesCommand;
-use Doctrine\ORM\Tools\Console\Command\GenerateRepositoriesCommand;
 use Doctrine\ORM\Tools\Console\Command\InfoCommand;
 use Doctrine\ORM\Tools\Console\Command\MappingDescribeCommand;
 use Doctrine\ORM\Tools\Console\Command\RunDqlCommand;
@@ -55,7 +50,7 @@ class ConfigProvider
                     'doctrine.entity_manager.orm_default' => EntityManagerFactory::class,
                     'doctrine.connection.orm_default' => [ConnectionFactory::class, 'orm_default'],
                 ],
-                'aliases' => [
+                'services' => [
                     EntityManagerInterface::class => 'doctrine.entity_manager.orm_default',
                     Connection::class => 'doctrine.connection.orm_default',
                 ],
@@ -63,7 +58,6 @@ class ConfigProvider
             'console' => [
                 'commands' => [
                     // DBAL Commands
-                    'dbal:import' => ImportCommand::class,
                     'dbal:reserved-words' => ReservedWordsCommand::class,
                     'dbal:run-sql' => RunSqlCommand::class,
 
@@ -78,9 +72,6 @@ class ConfigProvider
                     'orm:schema-tool:update' => UpdateCommand::class,
                     'orm:schema-tool:drop' => DropCommand::class,
                     'orm:ensure-production-settings' => EnsureProductionSettingsCommand::class,
-                    'orm:convert-d1-schema' => ConvertDoctrine1SchemaCommand::class,
-                    'orm:generate-repositories' => GenerateRepositoriesCommand::class,
-                    'orm:generate-entities' => GenerateEntitiesCommand::class,
                     'orm:generate-proxies' => GenerateProxiesCommand::class,
                     'orm:convert-mapping' => ConvertMappingCommand::class,
                     'orm:run-dql' => RunDqlCommand::class,
@@ -98,49 +89,43 @@ class ConfigProvider
                     'migrations:up-to-date' => UpToDateCommand::class,
                     'migrations:diff' => DiffCommand::class,
                 ],
-                'dependencies' => [
-                    'invokables' => [
-                        // DBAL Commands
-                        ImportCommand::class => ImportCommand::class,
-                        ReservedWordsCommand::class => ReservedWordsCommand::class,
-                        RunSqlCommand::class => RunSqlCommand::class,
+                'services' => [
+                    // DBAL Commands
+                    ReservedWordsCommand::class => ReservedWordsCommand::class,
+                    RunSqlCommand::class => RunSqlCommand::class,
 
-                        // ORM Commands
-                        CollectionRegionCommand::class => CollectionRegionCommand::class,
-                        EntityRegionCommand::class => EntityRegionCommand::class,
-                        MetadataCommand::class => MetadataCommand::class,
-                        QueryCommand::class => QueryCommand::class,
-                        QueryRegionCommand::class => QueryRegionCommand::class,
-                        ResultCommand::class => ResultCommand::class,
-                        CreateCommand::class => CreateCommand::class,
-                        UpdateCommand::class => UpdateCommand::class,
-                        DropCommand::class => DropCommand::class,
-                        EnsureProductionSettingsCommand::class => EnsureProductionSettingsCommand::class,
-                        ConvertDoctrine1SchemaCommand::class => ConvertDoctrine1SchemaCommand::class,
-                        GenerateRepositoriesCommand::class => GenerateRepositoriesCommand::class,
-                        GenerateEntitiesCommand::class => GenerateEntitiesCommand::class,
-                        GenerateProxiesCommand::class => GenerateProxiesCommand::class,
-                        ConvertMappingCommand::class => ConvertMappingCommand::class,
-                        RunDqlCommand::class => RunDqlCommand::class,
-                        ValidateSchemaCommand::class => ValidateSchemaCommand::class,
-                        InfoCommand::class => InfoCommand::class,
-                        MappingDescribeCommand::class => MappingDescribeCommand::class,
-                        DumpSchemaCommand::class => DumpSchemaCommand::class,
-                        ExecuteCommand::class => ExecuteCommand::class,
-                        GenerateCommand::class => GenerateCommand::class,
-                        LatestCommand::class => LatestCommand::class,
-                        MigrateCommand::class => MigrateCommand::class,
-                        RollupCommand::class => RollupCommand::class,
-                        StatusCommand::class => StatusCommand::class,
-                        VersionCommand::class => VersionCommand::class,
-                        UpToDateCommand::class => UpToDateCommand::class,
-                        DiffCommand::class => DiffCommand::class,
-                    ],
-                    'delegators' => [
-                        Console::class => [
-                            AntidotCliDoctrineDelegatorFactory::class
-                        ]
-                    ],
+                    // ORM Commands
+                    CollectionRegionCommand::class => CollectionRegionCommand::class,
+                    EntityRegionCommand::class => EntityRegionCommand::class,
+                    MetadataCommand::class => MetadataCommand::class,
+                    QueryCommand::class => QueryCommand::class,
+                    QueryRegionCommand::class => QueryRegionCommand::class,
+                    ResultCommand::class => ResultCommand::class,
+                    CreateCommand::class => CreateCommand::class,
+                    UpdateCommand::class => UpdateCommand::class,
+                    DropCommand::class => DropCommand::class,
+                    EnsureProductionSettingsCommand::class => EnsureProductionSettingsCommand::class,
+                    GenerateProxiesCommand::class => GenerateProxiesCommand::class,
+                    ConvertMappingCommand::class => ConvertMappingCommand::class,
+                    RunDqlCommand::class => RunDqlCommand::class,
+                    ValidateSchemaCommand::class => ValidateSchemaCommand::class,
+                    InfoCommand::class => InfoCommand::class,
+                    MappingDescribeCommand::class => MappingDescribeCommand::class,
+                    DumpSchemaCommand::class => DumpSchemaCommand::class,
+                    ExecuteCommand::class => ExecuteCommand::class,
+                    GenerateCommand::class => GenerateCommand::class,
+                    LatestCommand::class => LatestCommand::class,
+                    MigrateCommand::class => MigrateCommand::class,
+                    RollupCommand::class => RollupCommand::class,
+                    StatusCommand::class => StatusCommand::class,
+                    VersionCommand::class => VersionCommand::class,
+                    UpToDateCommand::class => UpToDateCommand::class,
+                    DiffCommand::class => DiffCommand::class,
+                ],
+                'delegators' => [
+                    Console::class => [
+                        AntidotCliDoctrineDelegatorFactory::class
+                    ]
                 ],
             ],
             'doctrine' => [
